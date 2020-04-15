875a34ca9d1cc63019e4df9f70d7d00f2daad574
TuongTLC <trinhtuong98@gmail.com>
2020-04-15 08:33:54 +0700

    1st upload

---
+++ src/swe102/SWE102.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swe102;
+import utils.MyToys;
+/**
+ *
+ * @author trinh
+ */
+public class SWE102 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("5! = " + MyToys.cF(5));
+        System.out.println("6! = " + MyToys.cF(6));
+        System.out.println("0! = " + MyToys.cF(0));
+        System.out.println("-5! = " + MyToys.cF(-5));
+    }
+    
+}
